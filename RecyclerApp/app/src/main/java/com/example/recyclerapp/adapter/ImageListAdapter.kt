ad24e4440ce3da086eab458487b094348e9bf7dc
Versilis Tyson <VersilisTyson@gmail.com>
2019-08-13 19:15:46 -0400

    adds ImageListAdapter

--- RecyclerApp/app/src/main/java/com/example/recyclerapp/adapter/ImageListAdapter.kt
+++ RecyclerApp/app/src/main/java/com/example/recyclerapp/adapter/ImageListAdapter.kt
@@ -1,27 +1,35 @@
 package com.example.recyclerapp.adapter
 
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerapp.R
 import com.example.recyclerapp.model.ImageData
 import kotlinx.android.synthetic.main.image_item_layout.view.*
 
 class ImageListAdapter(val data: MutableList<ImageData>): RecyclerView.Adapter<ImageListAdapter.ViewHolder>() {
 
-    class ViewHolder(view: ViewGroup): RecyclerView.ViewHolder(view) {
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
         val image: ImageView = view.iv
         val name: TextView = view.image_name
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-
+        val viewGroup = LayoutInflater.from(parent.context).inflate(R.layout.image_item_layout, parent, false)
+        ViewHolder(viewGroup)
+        return ViewHolder(viewGroup)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        holder.image.setImageURI(Uri.parse(data[position].toString()))
+        holder.name.text = data[position].toString()
     }
 }
